296a4f3f4005a4ceb3fd7b9a185e7c5c81ec88bb
IFU <tntcorporation@mail.ru>
2022-08-25 00:23:51 +0500

    no msg

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wedding',
         'USER': 'postgres',
